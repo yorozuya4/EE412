dae8416e863f8db53cc908ecc6ea775a525e550f
Y.H <108125850+yorozuya4@users.noreply.github.com>
2023-12-28 09:34:55 +0900

    Update hw1_1.py

--- hw1_1.py
+++ hw1_1.py
@@ -1,44 +1,36 @@
 import sys
-##import time
-
-##begin=time.time()
-
 from pyspark import SparkConf, SparkContext
-conf=SparkConf()
-sc=SparkContext(conf=conf)
 
-def potential_pair(friends):
-    key_pairs=[]
-    n=len(friends)
-    for i in range(n):
-        for j in range(i+1,n):
-            key_pair=tuple(sorted((friends[i],friends[j])))
-            key_pairs.append((key_pair,1))
-    return key_pairs
+def main(file_path):
+    conf = SparkConf()
+    with SparkContext(conf=conf) as sc:
+        raw_line = sc.textFile(file_path)
+        line = raw_line.map(lambda line: line.split("\t"))
+        real_friends = line.filter(lambda x: len(x) > 1)
+        user_friends = real_friends.map(lambda x: (x[0], x[1].split(",")))
 
-def direct_pair(user,friends):
-    key_pairs=[]
-    for friend in friends:
-        key_pair=tuple(sorted((user,friend)))
-        key_pairs.append(key_pair)
-    return key_pairs
+        # Create direct_pairs using a broadcast variable
+        direct_pairs = user_friends.flatMap(lambda x: [tuple(sorted((x[0], friend))) for friend in x[1]]).collect()
+        broadcast_direct_pairs = sc.broadcast(set(direct_pairs))
 
-raw_line=sc.textFile(sys.argv[1])
-line=raw_line.map(lambda line: line.split("\t"))
-real_friends=line.filter(lambda x: len(x)>1)
-user_friends=real_friends.map(lambda x: (x[0], x[1].split(",")))
-potential_pairs=user_friends.flatMap(lambda x: potential_pair(x[1]))
-direct_pairs=user_friends.flatMap(lambda x: direct_pair(x[0],x[1])).collect()
-direct_pairs=set(direct_pairs)
-mutual_pairs=potential_pairs.filter(lambda x: x[0] not in direct_pairs)
-mutual_friends=mutual_pairs.reduceByKey(lambda x,y: x+y)
+        def potential_pair(friends):
+            return [tuple(sorted((friends[i], friends[j]))) for i in range(len(friends)) for j in range(i + 1, len(friends))]
 
-results=mutual_friends.sortBy(lambda x: (-x[1],x[0][0],x[0][1]))
-top_ten=results.take(10)
-for ((user1,user2), count) in top_ten:
-    print(f"{user1}\t{user2}\t{count}")
+        potential_pairs = user_friends.flatMap(lambda x: potential_pair(x[1]))
+        mutual_pairs = potential_pairs.filter(lambda x: x not in broadcast_direct_pairs.value)
+        mutual_friends = mutual_pairs.reduceByKey(lambda x, y: x + y)
 
-sc.stop()
+        results = mutual_friends.sortBy(lambda x: (-x[1], x[0][0], x[0][1]))
+        top_ten = results.take(10)
+        for ((user1, user2), count) in top_ten:
+            print(f"{user1}\t{user2}\t{count}")
 
-##end=time.time()
-##print(f"Total time: {(end-begin):.6f} seconds")
+begin = time.time()
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: script.py <file>")
+        sys.exit(1)
+    main(sys.argv[1])
+end = time.time()
+
+print(f"Total time: {(end - begin):.6f} seconds")
