5b630d1b80b240dd1775a5eea22c0ea8c214c6ce
Y.H <108125850+yorozuya4@users.noreply.github.com>
2023-12-28 09:43:15 +0900

    Update hw1_3.py

--- hw1_3.py
+++ hw1_3.py
@@ -1,101 +1,66 @@
 import sys
-##import time
+import time
 import math
 import numpy as np
 
-##begin=time.time()
+begin = time.time()
 
-threshold=0.9
-rows=20
-bands=6
-sig_len=rows*bands
+threshold = 0.9
+rows = 20
+bands = 6
+sig_len = rows * bands
 
 def extract_shingles(article):
-    cleaned_article=""
-    for char in article:
-        if char.isalpha() or char.isspace():
-            cleaned_article+=char
-    cleaned_article=" ".join(cleaned_article.split()).lower()
-    shingles_set=set()
-    for i in range(len(cleaned_article)-2):
-        shingle=cleaned_article[i:i+3]
-        shingles_set.add(shingle)
-    return shingles_set
+    cleaned_article = ''.join(char.lower() for char in article if char.isalpha() or char.isspace())
+    return {''.join(cleaned_article[i:i+3]) for i in range(len(cleaned_article)-2)}
 
-def jaccard(s1,s2):
-    set1=set(s1)
-    set2=set(s2)
-    intersection=len(set1.intersection(set2))
-    union=len(set1.union(set2))
-    return intersection/union
+def jaccard(s1, s2):
+    intersection = len(s1 & s2)
+    union = len(s1 | s2)
+    return intersection / union
 
-id_list=list()
-unique_shingles=set()
-all_shingles=list()
-with open(sys.argv[1],'r') as articles:
+id_list = []
+unique_shingles = set()
+all_shingles = []
+with open(sys.argv[1], 'r') as articles:
     for article in articles:
-        id,document=article.split(" ",1)
+        id, document = article.split(" ", 1)
         id_list.append(id)
-        shingles=extract_shingles(document)
+        shingles = extract_shingles(document)
         all_shingles.append(shingles)
-        unique_shingles=unique_shingles.union(shingles)
+        unique_shingles |= shingles
 
-shingle_index=dict()
-for index,shingle in enumerate(unique_shingles):
-    shingle_index[shingle]=index
+shingle_index = {shingle: idx for idx, shingle in enumerate(unique_shingles)}
 
-C=len(unique_shingles)
-while True:
-    if all(C%i for i in range(2,int(math.sqrt(C))+1)):
-        break
-    else:
-        C+=1
+C = next(C for C in range(len(unique_shingles), sys.maxsize) if all(C % i for i in range(2, int(math.sqrt(C)) + 1)))
 
-col=len(id_list)
-A=np.random.randint(0,C,sig_len)
-B=np.random.randint(0,C,sig_len)
-signatures=np.full((col,sig_len),10**100)
-for idx in range(len(all_shingles)):
-    for shingle in all_shingles[idx]:
-        x=shingle_index[shingle]
-        hash_value=(A*x+B)%C
-        signatures[idx]=np.minimum(signatures[idx],hash_value)
+col = len(id_list)
+A = np.random.randint(0, C, sig_len)
+B = np.random.randint(0, C, sig_len)
+signatures = np.full((col, sig_len), float('inf'))
+for idx, shingles in enumerate(all_shingles):
+    for shingle in shingles:
+        x = shingle_index[shingle]
+        hash_values = (A * x + B) % C
+        signatures[idx] = np.minimum(signatures[idx], hash_values)
 
-candidates=set()
-hash_tables=list()
-for i in range(bands):
-    hash_tables.append(dict())
-
-for idx in range(len(signatures)):
+candidates = set()
+hash_tables = [{} for _ in range(bands)]
+for idx, signature in enumerate(signatures):
     for band in range(bands):
-        band_list=list()
-        for r in range(band*rows,(band+1)*rows):
-            band_list.append(signatures[idx][r])
-        band_data=tuple(band_list)
-
-        hash_val=0
-        for x in band_data:
-            value=(A[band]*x+B[band])%C
-            hash_val+=value
-        if hash_val not in hash_tables[band]:
-            hash_tables[band][hash_val]=list()
-        hash_tables[band][hash_val].append(idx)
+        band_data = tuple(signature[band * rows : (band + 1) * rows])
+        hash_val = sum((A[band] * x + B[band]) % C for x in band_data)
+        hash_tables[band].setdefault(hash_val, []).append(idx)
 
 for table in hash_tables:
     for bucket in table.values():
-        if len(bucket)>1:
-            for i in range(len(bucket)):
-                for j in range(i+1,len(bucket)):
-                    candidates.add((bucket[i],bucket[j]))
+        if len(bucket) > 1:
+            candidates.update((min(pair), max(pair)) for pair in itertools.combinations(bucket, 2))
 
-results=list()
-for i,j in candidates:
-    jaccard_sim=jaccard(all_shingles[i],all_shingles[j])
-    if jaccard_sim>=threshold:
-        results.append((i,j))
+results = [(id_list[i], id_list[j]) for i, j in candidates if jaccard(all_shingles[i], all_shingles[j]) >= threshold]
 
-for i,j in results:
-    print(f"{id_list[i]}\t{id_list[j]}")
+for i, j in results:
+    print(f"{i}\t{j}")
 
-##end=time.time()
-##print(f"Total time: {(end-begin):.6f} seconds")
+end = time.time()
+print(f"Total time: {(end - begin):.6f} seconds")
