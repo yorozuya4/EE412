fd0ae0718589813384835a3cfaf2cf5d535821fb
Y.H <108125850+yorozuya4@users.noreply.github.com>
2023-12-28 09:42:02 +0900

    Update hw1_2_c.py

--- hw1_2_c.py
+++ hw1_2_c.py
@@ -1,72 +1,51 @@
 import sys
-##import time
+import time
 
-##begin=time.time()
+threshold = 100
 
-threshold=100
+begin = time.time()
 
-item_baskets=list()
-with open(sys.argv[1],"r") as baskets:
-    for basket in baskets:
-        item_baskets.append(basket.strip().split())
+# Reading the baskets file and populating the item_baskets list
+with open(sys.argv[1], "r") as baskets:
+    item_baskets = [basket.strip().split() for basket in baskets]
 
-item_integer=dict()
-n=1
+# Generating item_integer and index_to_item dictionaries
+item_integer = {item: idx + 1 for idx, item in enumerate(set(item for basket in item_baskets for item in basket))}
+index_to_item = {idx: item for item, idx in item_integer.items()}
+
+# Counting pairs
+pair_counts = {}
 for basket in item_baskets:
-    for item in basket:
-        if item not in item_integer:
-            item_integer[item]=n
-            n+=1
+    indexed_basket = [item_integer[item] for item in basket]
+    for i in range(len(indexed_basket)):
+        for j in range(i + 1, len(indexed_basket)):
+            pair = tuple(sorted([indexed_basket[i], indexed_basket[j]]))
+            pair_counts[pair] = pair_counts.get(pair, 0) + 1
 
-index_to_item=dict()
-for item,index in item_integer.items():
-    index_to_item[index]=item
+# Identifying frequent pairs
+frequent_pairs = {pair: count for pair, count in pair_counts.items() if count >= threshold}
 
-pair_counts=dict()
+# Counting triples
+triple_counts = {}
 for basket in item_baskets:
-    index=[item_integer[item] for item in basket]
-    for i in range(len(index)):
-        for j in range(i+1,len(index)):
-            pair=tuple(sorted([index[i],index[j]]))
-            if pair in pair_counts:
-                pair_counts[pair]+=1
-            else:
-                pair_counts[pair]=1
+    indexed_basket = [item_integer[item] for item in basket]
+    for i in range(len(indexed_basket)):
+        for j in range(i + 1, len(indexed_basket)):
+            for k in range(j + 1, len(indexed_basket)):
+                triple = tuple(sorted([indexed_basket[i], indexed_basket[j], indexed_basket[k]]))
+                if all(tuple(sorted(pair)) in frequent_pairs for pair in [(triple[0], triple[1]), (triple[0], triple[2]), (triple[1], triple[2])]):
+                    triple_counts[triple] = triple_counts.get(triple, 0) + 1
 
-frequent_pairs=dict()
-for pair,count in pair_counts.items():
-    if count>=threshold:
-        frequent_pairs[pair]=count
+# Identifying frequent triples
+frequent_triples = {triple: k for triple, k in triple_counts.items() if k >= threshold}
 
-triple_counts=dict()
-for basket in item_baskets:
-    index=list()
-    for item in basket:
-        index.append(item_integer[item])
-    for i in range(len(index)):
-        for j in range(i+1,len(index)):
-            for k in range(j+1,len(index)):
-                if tuple(sorted([index[i],index[j]])) in frequent_pairs and tuple(sorted([index[i],index[k]])) in frequent_pairs and tuple(sorted([index[j],index[k]])) in frequent_pairs:
-                    triple=tuple(sorted([index[i],index[j],index[k]]))
-                    if triple in triple_counts:
-                        triple_counts[triple]+=1
-                    else:
-                        triple_counts[triple]=1
+# Sorting and displaying top ten results
+top_ten_frequent_triples = sorted(frequent_triples.items(), key=lambda x: -x[1])[:10]
+for (item1, item2, item3), count in top_ten_frequent_triples:
+    conf_12_3 = count / frequent_pairs[(item1, item2)]
+    conf_13_2 = count / frequent_pairs[(item1, item3)]
+    conf_23_1 = count / frequent_pairs[(item2, item3)]
+    print(f"{index_to_item[item1]}\t{index_to_item[item2]}\t{index_to_item[item3]}\t{count}\t{conf_12_3:.6f}\t{conf_13_2:.6f}\t{conf_23_1:.6f}")
 
-frequent_triples=dict()
-for triple,k in triple_counts.items():
-    if k>=threshold:
-        frequent_triples[triple]=k
-
-frequent_triples_list=list(frequent_triples.items())
-frequent_triples_list=sorted(frequent_triples_list, key=lambda x: -x[1])
-
-print(len(frequent_triples_list))
-for (item1,item2,item3),n in frequent_triples_list[:10]:
-    conf_12_3=n/frequent_pairs[(item1,item2)]
-    conf_13_2=n/frequent_pairs[(item1,item3)]
-    conf_23_1=n/frequent_pairs[(item2,item3)]
-    print(f"{index_to_item[item1]}\t{index_to_item[item2]}\t{index_to_item[item3]}\t{n}\t{conf_12_3:.6f}\t{conf_13_2:.6f}\t{conf_23_1:.6f}")
-
-##end=time.time()
-##print(f"Total time: {(end-begin):.6f} seconds")
+end = time.time()
+print(f"Total time: {(end - begin):.6f} seconds")
